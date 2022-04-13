1533571ec2e677adb4a49b2853dfe1751ff5a78e
ManoverKamaal <manover786kamaal@gmail.com>
2022-04-13 08:06:51 +0530

    changes in stoock

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from '../services/cart.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public totalItem : number =  20;
+  constructor(private cartService : CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getProducts()
+    .subscribe(res=>{
+      this.totalItem -= res.length;
+    })
   }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,40 +9,44 @@ import { CartService } from '../services/cart.service';
 })
 export class ProductsComponent implements OnInit {
 
-  public productList : any ;
-  public filterCategory : any
-  searchKey:string ="";
-  constructor(private api : ApiService, private cartService : CartService) { }
+  public productList: any;
+  public filterCategory: any
+  searchKey: string = "";
+  constructor(private api: ApiService, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.api.getProduct()
-    .subscribe(res=>{
-      this.productList = res;
-      this.filterCategory = res;
-      this.productList.forEach((a:any) => {
-        if(a.category ==="women's clothing" || a.category ==="men's clothing"){
-          a.category ="fashion"
-        }
-        Object.assign(a,{quantity:1,total:a.price});
+      .subscribe(res => {
+        this.productList = res;
+        this.filterCategory = res;
+        this.productList.forEach((a: any) => {
+          if (a.category === "women's clothing" || a.category === "men's clothing") {
+            a.category = "fashion"
+          }
+          Object.assign(a, { quantity: 1, total: a.price });
+        });
+        console.log(this.productList)
       });
-      console.log(this.productList)
-    });
 
-    this.cartService.search.subscribe((val:any)=>{
+    this.cartService.search.subscribe((val: any) => {
       this.searchKey = val;
     })
   }
-  addtocart(item: any){
+  addtocart(item: any) {
     this.cartService.addtoCart(item);
+    if (item.quantity != 2) {
+      item.quantity += 1;
+    }
   }
-  filter(category:string){
+  filter(category: string) {
     this.filterCategory = this.productList
-    .filter((a:any)=>{
-      if(a.category == category || category==''){
-        return a;
-      }
-    })
+      .filter((a: any) => {
+        if (a.category == category || category == '') {
+          return a;
+        }
+      })
   }
 
+  
 }
 
